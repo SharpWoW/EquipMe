8d0222f25c60bbe71b355df488c74747cc208d8a
Adam Hellberg <sharparam@sharparam.com>
2019-09-16 16:53:36 +0200

    Improve linting

---
+++ lint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+err=0
+err_handler() {
+  err=1
+}
+trap err_handler ERR
+
+moonc lint_config.moon
+
+echo '## BEGIN LUACHECK LINTS ##'
+
+luacheck . -q --codes
+
+echo '## END LUACHECK LINTS ##'
+echo '## BEGIN MOONPICK LINTS ##'
+
+for f in `find src spec out -type f -name "*.moon"`; do
+  moonpick "$f"
+done
+
+echo '## END MOONPICK LINTS ##'
+
+exit $err
